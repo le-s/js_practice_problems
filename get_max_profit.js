9c72c17e7baec895c016a83bf50d1be635908104
Steven Le <steven.an.le@gmail.com>
2019-03-13 15:38:07 -0700

    Work on get_max_profit problem

---
+++ get_max_profit.js
@@ -0,0 +1,8 @@
+function getMaxProfit(stockPrices) {
+  console.log(stockPrices);
+}
+
+const stockPrices = [10, 7, 5, 8, 11, 9];
+
+getMaxProfit(stockPrices);
+// Returns 6 (buying for $5 and selling for $11)
