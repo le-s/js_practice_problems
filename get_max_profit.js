cb1c52ad4dff9c32bbf711f226aac42a207cd24d
Steven Le <steven.an.le@gmail.com>
2019-03-14 14:53:16 -0700

    Finish get_max_profit problem

--- get_max_profit.js
+++ get_max_profit.js
@@ -1,8 +1,40 @@
 function getMaxProfit(stockPrices) {
-  console.log(stockPrices);
+  if (stockPrices.length < 0) return [];
+  let minimum;
+  let currentProfit;
+  let maxProfit;
+  let bestCase;
+
+  for (let i = 0; i < stockPrices.length; i++) {
+    
+    if (!minimum || stockPrices[i] < minimum) {
+      minimum = stockPrices[i];
+    }
+    
+    if (minimum < stockPrices[i]) {
+      currentProfit = stockPrices[i] - minimum;
+    }
+
+    if (!maxProfit || maxProfit < currentProfit) {
+      maxProfit = currentProfit;
+    }
+  }
+
+  for (let i = 0; i < stockPrices.length - 1; i++) {
+    let currentCase = stockPrices[i + 1] - stockPrices[i];
+    if (!bestCase || currentCase > bestCase) {
+      bestCase = currentCase;
+    }
+  }
+  
+  if (maxProfit) {
+    return maxProfit;
+  } else {
+    return bestCase;
+  }
 }
 
-const stockPrices = [10, 7, 5, 8, 11, 9];
+const stockPrices = [9, 7, 4, 1];
 
 getMaxProfit(stockPrices);
 // Returns 6 (buying for $5 and selling for $11)
