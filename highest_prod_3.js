8af5659622d26442420cb244dc8b57d71ee1980f
Steven Le <steven.an.le@gmail.com>
2019-03-18 14:13:02 -0700

    Brute force answer

--- highest_prod_3.js
+++ highest_prod_3.js
@@ -1,7 +1,19 @@
 function highestProductOf3(arrayOfInts) {
 
   // Calculate the highest product of three numbers
+  if (arrayOfInts.length <= 2) {
+    throw new Error("Too small")
+  }
+  arrayOfInts.sort((a, b) => {
+    return a - b;
+  })
 
+  let num = 1
 
-  return 0;
+  return arrayOfInts.slice(arrayOfInts.length - 3).reduce((acc, val) => {
+
+    return acc * val;
+  }, num)
 }
+
+highestProductOf3([1,2,3,4])
