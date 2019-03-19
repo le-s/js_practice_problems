2f9c1a8fd2ac5106dc859169482b37c2d442b324
Steven Le <steven.an.le@gmail.com>
2019-03-18 18:06:40 -0700

    Get brute force answer

--- rotating_dictionary.js
+++ rotating_dictionary.js
@@ -1,7 +1,25 @@
 function findRotationPoint(words) {
+  let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split("");
+  let hash = {};
+  let word = words[0];
+
+  for (let i = 0; i < alphabet.length; i++) {
+    hash[alphabet[i]] = i;
+  }
 
   // Find the rotation point in the vector
+  for (let i = 1; i < words.length; i++) {
+    if (hash[words[i][0]] < hash[word][0]) {
+      console.log(i);
+      return i;
+    }
 
+    if (hash[words[i][0]] = hash[word[0]]) {
+      return i;
+    }
 
-  return false;
+    word = words[i];
+  }
 }
+
+findRotationPoint(["grape", "orange", "plum", "radish", "apple"]);
