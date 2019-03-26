6abf09418bfcdff0da55d435acd41582a237be64
Steven Le <steven.an.le@gmail.com>
2019-03-26 15:49:24 -0700

    Create skeleton for get_perm problem

---
+++ get_perm.js
@@ -0,0 +1,10 @@
+function getPermutations(string) {
+
+  // Generate all permutations of the input string
+
+  let perms = new Set();
+
+  for (let i = 0; i < string.length; i++) {
+
+  }
+}
--- is_super_balanced.js
+++ is_super_balanced.js
@@ -16,10 +16,32 @@ class BinaryTreeNode {
   }
 }
 
-function isBalanced(treeRoot) {
+function isBalanced(treeRoot, depth=0) {
 
   // Determine if the tree is superbalanced
+  let lowestDepth;
 
+  if (!treeRoot.left && !treeRoot.right) {
+    if (lowestDepth && lowestDepth + 2 > depth) {
+      return false;
+    }
 
-  return false;
+    if (!lowestDepth || lowestDepth > depth) {
+      lowestDepth = depth;
+    }
+  }
+
+  isBalanced(treeRoot.left, depth + 1)
+  isBalanced(treeRoot.right, depth + 1)
+  return true;
 }
+
+let treeRoot = new BinaryTreeNode(5);
+let leftNode = treeRoot.insertLeft(8);
+leftNode.insertLeft(1);
+leftNode.insertRight(2);
+let rightNode = treeRoot.insertRight(6);
+rightNode.insertLeft(3);
+rightNode.insertRight(4);
+
+isBalanced(treeRoot);
