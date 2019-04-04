9b81be19f4ed4f2fd04f663acd77f4875c60e8b6
Steven Le <steven.an.le@gmail.com>
2019-04-03 17:38:19 -0700

    Solve searchRange in O(log n) time complexity

--- search_range.js
+++ search_range.js
@@ -1,3 +1,47 @@
 const searchRange = (nums, target) => {
+  let middleIndex = Math.floor(nums.length / 2);
+  let array = [];
+  let reverse = true;
 
+  while (reverse) {
+    if (nums[middleIndex - 1] !== target) {
+      reverse = false;
+    } else {
+      middleIndex = middleIndex - 1
+    }
+  }
+
+  if (target >= nums[middleIndex]) {
+
+    for (let i = middleIndex; i < nums.length; i++) {
+      if (nums[i] === target) {
+        if (array.length == 2) {
+          array.pop();
+        }
+
+        array.push(i);
+      }
+    }
+  } else {
+    for (let i = 0; i < middleIndex; i++) {
+      if (nums[i] === target) {
+        if (array.length == 2) {
+          array.pop();
+        }
+
+        array.push(i);
+      }
+    }
+  }
+
+
+  if (array.length === 0) {
+    array = [-1, -1];
+  }
+
+  if (array.length === 1) {
+    array.push(array[0]);
+  }
+
+  return array;
 };
