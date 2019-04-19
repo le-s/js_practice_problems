ba4e40a826ac07c9101609be0a582de4efb0f20f
Steven Le <steven.an.le@gmail.com>
2019-04-18 17:31:43 -0700

    Solve fib problem

--- fibs.js
+++ fibs.js
@@ -1,7 +1,20 @@
 function fib(n) {
-
   // Compute the nth Fibonacci number
-
-
+  if (n === 0) return 0;
+  if (n === 1) return 1;
   
+  if (memo.hasOwnProperty(n)) {
+    return memo[n];
+  }
+  
+  let result = fib((n - 1), memo) + fib((n - 2), memo);
+  
+  memo[n] = result;
+
+  return result;
 }
+
+// n = 3
+// fib(2) + fib(1)
+// fib(1) + fib(0) + 1
+// 1 + 0 + 1
