e918ac7315bc140cde0c35e147f450b7597d15d0
Steven Le <steven.an.le@gmail.com>
2019-04-19 17:36:08 -0700

    Complete fib problem bottom-up approach

--- fibs.js
+++ fibs.js
@@ -1,18 +1,37 @@
-function fib(n) {
-  // Compute the nth Fibonacci number
-  if (n === 0) return 0;
-  if (n === 1) return 1;
-  
-  if (memo.hasOwnProperty(n)) {
-    return memo[n];
-  }
-  
-  let result = fib((n - 1), memo) + fib((n - 2), memo);
-  
-  memo[n] = result;
+function fib(n){
+  if (n < 0) {
+    throw new Error('N cannot be negative')
+  } else if (n === 0 || n === 1) {
+    return n;
+  } 
+  let prevPrev = 0;
+  let prev = 1;
+  let current;
 
-  return result;
+  for (let i = 1; i < n; i++) {
+    current = prev + prevPrev;
+    prevPrev = prev;
+    prev = current;
+  }
+
+  return current;
 }
+// Memoization method
+// function fib(n) {
+//   // Compute the nth Fibonacci number
+//   if (n === 0) return 0;
+//   if (n === 1) return 1;
+  
+//   if (memo.hasOwnProperty(n)) {
+//     return memo[n];
+//   }
+  
+//   let result = fib((n - 1), memo) + fib((n - 2), memo);
+  
+//   memo[n] = result;
+
+//   return result;
+// }
 
 // n = 3
 // fib(2) + fib(1)
